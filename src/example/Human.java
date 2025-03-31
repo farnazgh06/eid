9d8c89b497af22476c4442a1781b9c7cd6a54d15
farnaz <whoisfarnaz@gmail.com>
2025-04-01 01:14:56 +0330

    add Human-Class

---
+++ src/example/Human.java
@@ -0,0 +1,23 @@
+package example;
+import db.Entity;
+
+public class Human extends Entity {
+    public String name;
+    public int age;
+
+    public Human(String name, int age ) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    @Override
+    public Human copy() {
+        Human copyHuman = new Human(name, age);
+        copyHuman.id = id;
+        copyHuman.age = age;
+
+        return copyHuman;
+    }
+
+}
