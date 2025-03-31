b52b59c95009726c197f7c77b30ce270c2b6fc36
farnaz <whoisfarnaz@gmail.com>
2025-04-01 01:19:15 +0330

    add getEntityCode to HumanClass

--- src/example/Human.java
+++ src/example/Human.java
@@ -4,6 +4,7 @@ import db.Entity;
 public class Human extends Entity {
     public String name;
     public int age;
+    public static final int HUMAN_ENTITY_CODE = 14;
 
     public Human(String name, int age ) {
         this.name = name;
@@ -19,5 +20,10 @@ public class Human extends Entity {
 
         return copyHuman;
     }
+    @Override
+    public int getEntityCode() {
+        return HUMAN_ENTITY_CODE;
+    }
+
 
 }
