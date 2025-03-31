50711592deac90107db611c0417d253ff12441d8
farnaz <whoisfarnaz@gmail.com>
2025-03-31 15:53:36 +0330

    add EntityNotFoundException

---
+++ src/db/EntityNotFoundException.java
@@ -0,0 +1,17 @@
+package db;
+
+
+public class EntityNotFoundException extends Exception{
+
+    public EntityNotFoundException(){
+        super("Cannot find entity");
+    }
+
+    public EntityNotFoundException(String message){
+        super(message);
+    }
+
+    public EntityNotFoundException(int id){
+        super("Cannot find entity with id=" + id);
+    }
+}
