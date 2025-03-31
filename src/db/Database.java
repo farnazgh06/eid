0482a8557b36ba94a570c462b3680a8049b09718
farnaz <whoisfarnaz@gmail.com>
2025-04-01 01:23:31 +0330

    chek the validator

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,12 +1,20 @@
 package db;
 
+import db.exception.InvalidEntityException;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Database {
     private static ArrayList<Entity> entities = new ArrayList<>();
     private static int IdCounter = 1;
+    private static HashMap<Integer, Validator> validators;
 
-    public static void add (Entity e){
+    public static void add (Entity e) throws InvalidEntityException {
+        Validator validator = validators.get(e.getEntityCode());
+        if (validator != null){
+            validator.validate(e);
+        }
         e.id = IdCounter++;
         entities.add(e.copy());
     }
@@ -29,7 +37,11 @@ public class Database {
         throw new EntityNotFoundException();
 
     }
-    public static void update(Entity e) throws EntityNotFoundException {
+    public static void update(Entity e) throws EntityNotFoundException, InvalidEntityException {
+        Validator validator = validators.get(e.getEntityCode());
+        if (validator != null){
+            validator.validate(e);
+        }
         for(Entity entity : entities) {
             if (entity.id == e.id) {
                 entities.remove(entity);
@@ -38,5 +50,11 @@ public class Database {
             }
         }
         throw new EntityNotFoundException();
+    }
+    public static void registerValidator(int entityCode, Validator validator) {
+        if (validators.get(entityCode) != null){
+            throw new IllegalArgumentException("there is a Validator!");
+        }
+        validators.put(entityCode,validator);
     }
 }
