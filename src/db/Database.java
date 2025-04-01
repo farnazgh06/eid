906455f2ce583391149ff6dffe088cfca95d99e8
farnaz <whoisfarnaz@gmail.com>
2025-04-01 20:55:35 +0330

    Update CreationDate and LastModificationDte in add and update methode

--- src/db/Database.java
+++ src/db/Database.java
@@ -3,6 +3,7 @@ package db;
 import db.exception.InvalidEntityException;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 
 public class Database {
@@ -11,13 +12,22 @@ public class Database {
     private static HashMap<Integer, Validator> validators = new HashMap<>();
 
     public static void add (Entity e) throws InvalidEntityException {
+        e.id = IdCounter++;
+
         Validator validator = validators.get(e.getEntityCode());
         if (validator != null){
             validator.validate(e);
         }
-        e.id = IdCounter++;
+        if (e instanceof Trackable){
+            Date date = new Date();
+            Trackable trackableE = (Trackable) e;
+            trackableE.setLastModificationDate(date);
+            trackableE.setCreationDate(date);
+        }
+
         entities.add(e.copy());
     }
+
     public static Entity get(int id) throws EntityNotFoundException {
         for (Entity entity : entities) {
             if (entity.id == id) {
@@ -34,22 +44,31 @@ public class Database {
                 return;
             }
         }
-        throw new EntityNotFoundException();
+        throw new EntityNotFoundException(id);
 
     }
     public static void update(Entity e) throws EntityNotFoundException, InvalidEntityException {
-        Validator validator = validators.get(e.getEntityCode());
-        if (validator != null){
-            validator.validate(e);
-        }
-        for(Entity entity : entities) {
+        for (Entity entity : entities) {
             if (entity.id == e.id) {
+
+                Validator validator = validators.get(e.getEntityCode());
+                if (validator != null) {
+                    validator.validate(e);
+                }
+
+                if (e instanceof Trackable){
+                    Date date = new Date();
+                    Trackable trackableE = (Trackable) e;
+                    trackableE.setLastModificationDate(new Date());
+                }
+
                 entities.remove(entity);
                 entities.add(e.copy());
+
                 return;
             }
         }
-        throw new EntityNotFoundException();
+        throw new EntityNotFoundException(e.id);
     }
     public static void registerValidator(int entityCode, Validator validator) {
         if (validators.get(entityCode) != null){
